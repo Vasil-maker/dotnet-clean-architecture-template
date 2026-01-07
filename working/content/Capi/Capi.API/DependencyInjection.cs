7f102656c70de4b8872ab9f972800468165843ec
Vasil <bryusovvasily@yandex.ru>
2026-01-07 14:26:03 +0300

    Добавьте файлы проекта.

---
+++ working/content/Capi/Capi.API/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection; 
+ 
+ 
+namespace Capi.API;
+ 
+public static class DependencyInjection
+{
+    // Два метода расширения 1-ый метод для регистрации сервисов 2-ой для конвигурации приложения
+
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+        
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+          
+        app.MapGet("/", () => "Hello World!");
+
+        return app;
+    }
+
+      
+}
---
+++ working/content/Capi/Capi.API/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿//Пустые файлы, которые будут содержать в себе только using
+
+//Нужно, чтобы в каждом классе не плодить кучу using
+
+global using System;
+global using Capi.API;
+global using Capi.Infrastructure;
+global using Capi.Application;
